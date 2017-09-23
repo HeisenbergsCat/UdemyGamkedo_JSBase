ef7783cd1aca8ec382fcac3f5050276973806db2
HeisenbergsCat <octavo@o2.pl>
2017-09-23 13:06:01 +0200

    box collisions wip

--- player.js
+++ player.js
@@ -3,69 +3,91 @@ const PLAYER_START = new Vector(1, 1);
 //PLAYER CONSTRUCTOR FUNCTION
 function Player(player) {
     this.pos = new Vector(100, 100);
-    this.accel = 8;
+    this.accel = 3;
     this.player = player;
-
     this.upPressed = false;
     this.downPressed = false;
     this.rightPressed = false;
     this.leftPressed = false;
     this.triggerPressed = false;
+    this.colboxSize = 7;
+
+    this.colBOX = {
+        A: new Vector(this.pos.X - this.colboxSize, this.pos.Y - this.colboxSize),
+        B: new Vector(this.pos.X + this.colboxSize, this.pos.Y + this.colboxSize),
+        C: new Vector(this.pos.X - this.colboxSize, this.pos.Y + this.colboxSize),
+        D: new Vector(this.pos.X + this.colboxSize, this.pos.Y - this.colboxSize)
+    };
 
     this.render = function() {
 
-        drawBitmapRotation(carPic, this.pos.X, this.pos.Y, this.rot);
+        //drawBitmapRotation(carPic, this.pos.X, this.pos.Y, this.rot);
         this.renderBox();
-    }
+    };
 
     this.carController = function() {
-
-
         if (this.rightPressed) {
             this.pos.X += this.accel;
-            if (this.collisionCheck()) {
-                var playerGridPos = this.getPlayerGridPosition();
-                var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
-                this.pos.X = collisionTile.colBox.X - 5;
-            }
         }
         if (this.leftPressed) {
             this.pos.X -= this.accel;
-            if (this.collisionCheck()) {
-                var playerGridPos = this.getPlayerGridPosition();
-                var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
-                this.pos.X = collisionTile.colBoxTrueEnd.X + 5;
-            }
         }
         if (this.upPressed) {
             this.pos.Y -= this.accel;
-            if (this.collisionCheck()) {
-                var playerGridPos = this.getPlayerGridPosition();
-                var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
-                this.pos.Y = collisionTile.colBoxTrueEnd.Y + 5;
-            }
         }
         if (this.downPressed) {
             this.pos.Y += this.accel;
-            if (this.collisionCheck()) {
-                var playerGridPos = this.getPlayerGridPosition();
-                var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
-                this.pos.Y = collisionTile.colBox.Y - 5;
-            }
         }
         if (this.triggerPressed) {
             console.log("fire!");
         }
-    }
+    };
+
+    this.updateColBOX = function() {
+        this.colBOX.A.X = this.pos.X - this.colboxSize;
+        this.colBOX.A.Y = this.pos.Y - this.colboxSize;
+
+        this.colBOX.B.X = this.pos.X + this.colboxSize;
+        this.colBOX.B.Y = this.pos.Y + this.colboxSize;
+
+        this.colBOX.C.X = this.pos.X - this.colboxSize;
+        this.colBOX.C.Y = this.pos.Y + this.colboxSize;
+
+        this.colBOX.D.X = this.pos.X + this.colboxSize;
+        this.colBOX.D.Y = this.pos.Y - this.colboxSize;
+    };
 
     this.updatePosition = function() {
-        this.wrapEdges();
         this.carController();
-    }
+        this.updateColBOX();
+        this.collide();
+        this.wrapEdges();
+    };
 
     this.reset = function(posx, posy) {
         this.pos.X = Math.floor(posx * GTILE_SIZE) + GTILE_SIZE / 2;
         this.pos.Y = Math.floor(posy * GTILE_SIZE) + GTILE_SIZE / 2;
+    };
+
+    this.collide = function() {
+        var playerGridPos = this.getPlayerGridPosition();
+        var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
+        switch (this.collisionCheck()) {
+            case "west":
+                this.pos.X = collisionTile.colBOX.A.X;
+                break;
+            case "east":
+                this.pos.X = collisionTile.colBOX.D.X;
+                break;
+            case "north":
+                this.pos.Y = collisionTile.colBOX.A.Y;
+                break;
+            case "south":
+                this.pos.Y = collisionTile.colBOX.C.Y;
+                break;
+            default:
+                break;
+        }
     }
 
     //collision check
@@ -74,37 +96,53 @@ function Player(player) {
         var collisionTile = World.world[playerGridPos.Y][playerGridPos.X];
         if (collisionTile != undefined) {
             if (collisionTile.collider == true) {
-                if (this.pos.Y + 5 > collisionTile.colBox.Y &&
-                    this.pos.X + 5 > collisionTile.colBox.X &&
-                    this.pos.X - 5 < collisionTile.colBox.X + (collisionTile.colBoxTrueEnd.X - collisionTile.pos.X) &&
-                    this.pos.Y - 5 < collisionTile.colBox.Y + (collisionTile.colBoxTrueEnd.Y - collisionTile.pos.Y)) {
-                    return true;
-                } else return false;
-            }
-        }
+                if (this.pos.Y > collisionTile.colBOX.A.Y &&
+                    this.pos.Y < collisionTile.colBOX.C.Y) {
+                    if (this.pos.X < collisionTile.colBOX.B.X && this.pos.X > collisionTile.colBOX.B.X - (this.accel + 1)) {
+                        console.log("east");
+                        return "east";
+                    }
+                    if (this.colBOX.D.X > collisionTile.colBOX.A.X && this.colBOX.D.X < collisionTile.colBOX.A.X + (this.accel + 1)) {
+                        console.log("west");
+                        return "west";
+                    }
+                }
+                if (this.pos.X > collisionTile.colBOX.A.X &&
+                    this.pos.X < collisionTile.colBOX.D.X) {
+                    if (this.pos.Y > collisionTile.colBOX.A.Y && this.pos.Y < collisionTile.colBOX.A.Y + (this.accel + 1)) {
+                        console.log("north");
+                        return "north";
+                    }
+                    if (this.pos.Y < collisionTile.colBOX.C.Y && this.pos.Y > collisionTile.colBOX.C.Y - (this.accel + 1)) {
+                        console.log("south");
+                        return "south";
+                    }
 
-    }
+                }
+
+            } else return false;
+        }
+    };
 
     this.getPlayerGridPosition = function() {
         var playerGridPos = new Vector();
         playerGridPos.X = Math.floor(this.pos.X / GTILE_SIZE);
         playerGridPos.Y = Math.floor(this.pos.Y / GTILE_SIZE);
         return playerGridPos;
-    }
+    };
 
     this.renderBox = function() {
-        drawRectangle(
-            this.pos.X - 5,
-            this.pos.Y - 5,
-            10,
-            10,
-            "red", "stroke");
-    }
+        drawCircle(this.pos.X, this.pos.Y, 1, "red", "stroke")
+        drawCircle(this.colBOX.A.X, this.colBOX.A.Y, 1, "yellow", "stroke")
+        drawCircle(this.colBOX.B.X, this.colBOX.B.Y, 1, "blue", "stroke")
+        drawCircle(this.colBOX.C.X, this.colBOX.C.Y, 1, "green", "stroke")
+        drawCircle(this.colBOX.D.X, this.colBOX.D.Y, 1, "white", "stroke")
+    };
 
     this.wrapEdges = function() {
         if (this.pos.Y < 0) { this.pos.Y = canvas.height - GTILE_SIZE; }
         if (this.pos.Y > canvas.height - GTILE_SIZE) { this.pos.Y = 0; }
         if (this.pos.X < 0) { this.pos.X = canvas.width; }
         if (this.pos.X > canvas.width) { this.pos.X = 0 }
-    }
+    };
 } // end of constructor function Car
--- tile.js
+++ tile.js
@@ -4,142 +4,125 @@ function Brick(posx, posy, type) {
     this.tileType = type;
     this.image = asphA;
     this.collider = false;
+    this.colboxSize = GTILE_SIZE;
 
-    this.colBox = new Vector(this.pos.X, this.pos.Y);
-    this.colBoxTrueEnd = new Vector(0, 0);
+
+    this.colBOX = {
+        A: new Vector(0, 0),
+        B: new Vector(0, 0),
+        C: new Vector(0, 0),
+        D: new Vector(0, 0)
+    };
 
     this.switchType = function() {
         switch (this.tileType) {
             case 0:
                 this.image = asphA;
+                this.updateColBOX();
                 this.collider = false;
                 break;
             case 1:
                 this.image = curb_R;
-                this.colBox.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 2:
                 this.image = curb_L;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 3:
                 this.image = curb_T;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 4:
                 this.image = curb_B;
-                this.colBox.Y = this.pos.Y + (this.BRICK_SIDE / 2);
-                this.colBox.X = this.pos.X;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 5:
                 this.image = corner_TR;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
                 this.collider = true;
                 break;
             case 6:
                 this.image = corner_TL;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
                 this.collider = true;
                 break;
             case 7:
                 this.image = corner_BR;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
                 this.collider = true;
                 break;
             case 8:
                 this.image = corner_BL;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
                 this.collider = true;
                 break;
             case 9:
                 this.image = cornerB_TR;
-                this.colBox.Y = this.pos.Y + (this.BRICK_SIDE / 2);
-                this.colBox.X = this.pos.X;
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
                 this.collider = true;
                 break;
             case 10:
                 this.image = cornerB_TL;
-                this.colBox.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBox.Y = this.pos.Y + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
                 this.collider = true;
                 break;
             case 11:
                 this.image = cornerB_BR;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 12:
                 this.image = cornerB_BL;
-                this.colBox.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + (this.BRICK_SIDE / 2);
-                this.colBoxTrueEnd.Y = this.pos.Y + (this.BRICK_SIDE / 2);
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 13:
                 this.image = grassA;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
+                this.updateColBOX();
                 this.collider = true;
                 break;
             case 14:
                 this.image = finish;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
-                this.colBoxTrueEnd.X = this.pos.X + this.BRICK_SIDE - 1;
-                this.colBoxTrueEnd.Y = this.pos.Y + this.BRICK_SIDE - 1;
+                this.updateColBOX();
                 this.collider = true;
                 break;
             default:
                 this.image = grassA;
-                this.colBox.X = this.pos.X;
-                this.colBox.Y = this.pos.Y;
                 this.collider = true;
         }
     }
 
+    this.updateColBOX = function() {
+        this.colBOX.A.X = this.pos.X;
+        this.colBOX.A.Y = this.pos.Y;
+
+        this.colBOX.B.X = this.pos.X + this.colboxSize;
+        this.colBOX.B.Y = this.pos.Y + this.colboxSize;
+
+        this.colBOX.C.X = this.pos.X;
+        this.colBOX.C.Y = this.pos.Y + this.colboxSize;
+
+        this.colBOX.D.X = this.pos.X + this.colboxSize;
+        this.colBOX.D.Y = this.pos.Y;
+    };
+
+    this.renderBox = function() {
+        showText("A", this.colBOX.A.X, this.colBOX.A.Y + 7, 1, "yellow");
+        showText("B", this.colBOX.B.X - 7, this.colBOX.B.Y, 1, "blue");
+        showText("C", this.colBOX.C.X, this.colBOX.C.Y, 1, "green");
+        showText("D", this.colBOX.D.X - 7, this.colBOX.D.Y + 7, 1, "white");
+
+    };
+
     //draws brick on the screen
     this.render = function() {
         canvasContext.drawImage(this.image, this.pos.X, this.pos.Y);
-        this.renderBox();
+        if (this.collider) {
+            this.renderBox();
+        }
     }
 
+
+    /*
     this.renderBox = function() {
         if (this.collider) {
             drawRectangle(
@@ -149,5 +132,5 @@ function Brick(posx, posy, type) {
                 this.colBoxTrueEnd.Y - this.pos.Y,
                 "yellow", "stroke");
         }
-    }
+    }*/
 }
